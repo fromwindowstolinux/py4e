4fe7bf6b54737257ff66c5259076c7d139d6a5f0
fromwindowstolinux <nhiroin@gmail.com>
2021-09-13 16:55:20 +0800

    updated

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,5 @@
+# Write a program that prints one line other than 'hello world'. 
+# You should use the command line to execute the Python program you wrote in the text editor. 
+# Please do *not* use the IDLE Python Shell, the Python Interpreter (>>>), or a shortcut in your text editor to run the code. 
+
 print("Try it! Open the Python Interpreter and type 'import this' (without the ')")
